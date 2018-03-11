d04b6c006bc6b8bf9d105d4b6b8755e52cd6e2ca
Noa Mazeh <noa1maze@gmail.com>
2018-03-11 11:44:09 +0200

    oh yea

---
+++ first.ps1
@@ -0,0 +1,18 @@
+﻿function Dolev-Beiza
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [int]$Num
+    )
+
+    Process
+    {
+        0..$Num | ForEach-Object {
+        write-host "Dolev = Gezer" -ForegroundColor darkcyan -BackgroundColor Black}
+    }
+}
